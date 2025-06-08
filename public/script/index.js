8f7e06a708bfce174b7e6a39e8300cd6b8a8d21f
Gal1h <turboch031@gmail.com>
2025-06-08 09:41:56 +0700

    test

--- public/script/index.js
+++ public/script/index.js
@@ -12,26 +12,6 @@ document.getElementById("burger").onclick = function()
     }
 }
 
-// WIP
-const switchbtn = document.getElementById("switch");
-document.getElementById("switch").onclick = function()
-{
-    let switchCircle = document.getElementById("switch-circle");
-    if (switchbtn.style.backgroundColor == "white") {
-        switchCircle.style.marginLeft = "25px";
-        switchbtn.style.backgroundColor = "black";
-        document.body.style.backgroundColor = "#3b3b3b";
-        document.body.style.color = "white";
-    }
-    else {
-        switchbtn.style.backgroundColor = "white";
-        switchCircle.style.marginLeft = "0px";
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-    }
-    
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const tripledots = document.querySelectorAll(".tripledot");
 
